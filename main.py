15f1c5e82bf92ed48014eba7d0459744edb2adcb
Mary Sprague <maryksprague@gmail.com>
2026-02-09 21:27:58 +0000

    Add main function and program entry point

--- main.py
+++ main.py
@@ -49,13 +49,18 @@ def start(best_buy: store.Store):
         else:
             print("Invalid choice, please try again.")
 
-if __name__ == "__main__":
-    # setup initial stock of inventory
+def main():
     product_list = [
         products.Product("MacBook Air M2", price=1450, quantity=100),
         products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
         products.Product("Google Pixel 7", price=500, quantity=250),
+        products.NonStockedProduct("Windows License", price=125),
+        products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1),
     ]
 
     best_buy = store.Store(product_list)
-    start(best_buy)
+    best_buy.start()
+
+
+if __name__ == "__main__":
+    main()
